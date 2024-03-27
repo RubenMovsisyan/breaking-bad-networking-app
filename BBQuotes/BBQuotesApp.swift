abdf9e1e1c8815810e60cf6a42f3be80b656159a
Ruben Movsisyan <rubenmovsisyan@Rubens-MacBook-Pro-2.local>
2024-03-26 21:20:26 -0700

    first commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Ruben Movsisyan on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
